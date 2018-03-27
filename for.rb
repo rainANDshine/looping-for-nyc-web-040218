2850d86b1c0858955a7606cacd3bb6b1d317469c
Shun Yao <ysmiracle@hotmail.com>
2018-03-27 00:43:15 +0000

    automatically backed up by learn

--- for.rb
+++ for.rb
@@ -2,7 +2,11 @@
 def using_for
 	checklist = 1..10
 	#your code here
+<<<<<<< HEAD
 	for item in checklist do
+=======
+	for item in checlist do
+>>>>>>> 7c2d7011b9afeeb54680669600dbddb71adf246d
 	  puts "Wingardium Leviosa"
 	end
 end
